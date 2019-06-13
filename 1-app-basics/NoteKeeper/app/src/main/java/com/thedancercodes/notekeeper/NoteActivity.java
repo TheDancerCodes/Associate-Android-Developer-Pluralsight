f35addd9dc59e5b72333329c7f31cf3da7bcd836
Roger Taracha <rojtaracha@gmail.com>
2019-06-13 19:00:44 +0300

    Activity/Layout Relationship: [+] Remove FAB from layout file.
    [+] Add a reference to our spinner in the Activity.

--- 1-app-basics/NoteKeeper/app/src/main/java/com/thedancercodes/notekeeper/NoteActivity.java
+++ 1-app-basics/NoteKeeper/app/src/main/java/com/thedancercodes/notekeeper/NoteActivity.java
@@ -1,13 +1,11 @@
 package com.thedancercodes.notekeeper;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Spinner;
 
 public class NoteActivity extends AppCompatActivity {
 
@@ -18,14 +16,8 @@ public class NoteActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // Reference to our spinner
+        Spinner spinnerCourses = findViewById(R.id.spinner_courses);
     }
 
     @Override
