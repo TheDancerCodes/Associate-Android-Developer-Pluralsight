213e42cb559211a16c5d09d8272a02abb4db2304
Roger Taracha <rojtaracha@gmail.com>
2019-06-13 11:44:45 +0300

    Restructure Project.

---
+++ 1-app-basics/FirstApp/app/src/main/java/com/thedancercodes/firstapp/MyWorker.java
@@ -0,0 +1,9 @@
+package com.thedancercodes.firstapp;
+
+public class MyWorker {
+    public static int doubleTheValue(int value){
+
+        // Takes value passed into it, multiplies by 2 and returns it.
+        return value * 2;
+    }
+}
