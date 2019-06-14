5403a730a5d3c803d58f0b5ef31b0afe912acab1
Roger Taracha <rojtaracha@gmail.com>
2019-06-14 09:06:03 +0300

    Implementing Activity Interaction and Startup Behaviour: [+] Using Intents for Activity Interaction.
    [+] Update Manifest file to implement correct start up beahviour.

--- 1-app-basics/NoteKeeper/app/src/main/java/com/thedancercodes/notekeeper/NoteListActivity.java
+++ 1-app-basics/NoteKeeper/app/src/main/java/com/thedancercodes/notekeeper/NoteListActivity.java
@@ -1,5 +1,6 @@
 package com.thedancercodes.notekeeper;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -63,6 +64,11 @@ public class NoteListActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+                // Create an intent that identifies the activity we want to start.
+                Intent intent = new Intent(NoteListActivity.this, NoteActivity.class);
+
+                // Launch the Activity
+                startActivity(intent);
             }
         });
 
