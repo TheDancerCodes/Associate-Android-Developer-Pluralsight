727a882aa34df7f26329e79669843fb3fbd40c6b
Roger Taracha <rojtaracha@gmail.com>
2019-06-18 13:45:32 +0300

    Pre-processing Between Each Test: [+] Ensure that every test runs in a consistent environment.

--- 2-android-tools-and-testing/NoteKeeper/app/src/test/java/com/thedancercodes/notekeeper/DataManagerTest.java
+++ 2-android-tools-and-testing/NoteKeeper/app/src/test/java/com/thedancercodes/notekeeper/DataManagerTest.java
@@ -1,13 +1,26 @@
 package com.thedancercodes.notekeeper;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class DataManagerTest {
 
+    /**
+     * This method runs before each test in this class
+     *
+     * Makes sure list of notes always starts in a consistent state
+     */
+    @Before
+    public void setUp() {
+        DataManager dm = DataManager.getInstance();
+        dm.getNotes().clear();
+        dm.initializeExampleNotes();
+    }
+
     @Test
-    public void createNewNote() throws Exception {
+    public void createNewNote() {
 
         // Local variable of DataManager instance
         DataManager dm = DataManager.getInstance();
@@ -37,5 +50,43 @@ public class DataManagerTest {
         assertEquals(noteTitle, compareNote.getTitle());
         assertEquals(noteText, compareNote.getText());
 
+    }
+
+    /**
+     * Very that our findNote behaves correctly in an edge case
+     */
+    @Test
+    public void findSimilarNotes() {
+
+        // Reference to DataManager
+        final DataManager dm = DataManager.getInstance();
+
+        // Initialize values for the 2 notes
+        final CourseInfo course = dm.getCourse("android_async");
+        final String noteTitle = "Test note title";
+        final String noteText1 = "This is the body text of my test note";
+        final String noteText2 = "This is the body text of my second test note";
+
+        // Create the 1st note
+        int noteIndex1 = dm.createNewNote();
+        NoteInfo newNote1 = dm.getNotes().get(noteIndex1);
+        newNote1.setCourse(course);
+        newNote1.setTitle(noteTitle);
+        newNote1.setText(noteText1);
+
+        // Create the 2nd note
+        int noteIndex2 = dm.createNewNote();
+        NoteInfo newNote2 = dm.getNotes().get(noteIndex2);
+        newNote2.setCourse(course);
+        newNote2.setTitle(noteTitle);
+        newNote2.setText(noteText2);
+
+        // Find 1st note & confirm we get back the index of where we put the 1st note
+        int foundIndex1 = dm.findNote(newNote1);
+        assertEquals(noteIndex1, foundIndex1);
+
+        // Find 2nd note & confirm we get back the index of where we put the 2nd note
+        int foundIndex2 = dm.findNote(newNote2);
+        assertEquals(noteIndex2, foundIndex2);
     }
 }
