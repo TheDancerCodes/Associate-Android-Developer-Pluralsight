441db4159825d80b9c41b4960ce38a6fafa65478
Roger Taracha <rojtaracha@gmail.com>
2019-06-19 12:53:41 +0300

    Implementing Test Verification: [+] Leverage ViewAssertions to add code that checks both UI behaviour and logic behaviour.

--- 2-android-tools-and-testing/NoteKeeper/app/src/androidTest/java/com/thedancercodes/notekeeper/NoteCreationTest.java
+++ 2-android-tools-and-testing/NoteKeeper/app/src/androidTest/java/com/thedancercodes/notekeeper/NoteCreationTest.java
@@ -23,6 +23,7 @@ import static android.support.test.espresso.action.ViewActions.*;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static org.hamcrest.Matchers.*;
 import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.assertion.ViewAssertions.*;
 
 @RunWith(AndroidJUnit4.class)
 public class NoteCreationTest {
@@ -75,15 +76,47 @@ public class NoteCreationTest {
         // CourseInfo instance which is equal to the course variable & select it.
         onData(allOf(instanceOf(CourseInfo.class), equalTo(course))).perform(click());
 
+        // UI Behaviour Check: Check that what is currently displayed by spinner is the course title
+        onView(withId(R.id.spinner_courses)).check(matches(withSpinnerText(
+                containsString(course.getTitle()))));
+
         // Get to our note title field of the NoteActivity & input text
-        onView(withId(R.id.text_note_title)).perform(typeText(noteTitle));
+        // Add check: Check that edit text contains the text we typed into it
+        onView(withId(R.id.text_note_title)).perform(typeText(noteTitle))
+                .check(matches(withText(containsString(noteTitle))));
 
         // Get to our note text field of the NoteActivity & input text
         onView(withId(R.id.text_note_text))
                 .perform(typeText(noteText),
                         closeSoftKeyboard());
 
+        // UI Behaviour Check: Check that edit text contains the text we typed into it
+        onView(withId(R.id.text_note_text)).check(matches(withText(containsString(noteText))));
+
         // Leave NoteActivity at the end of the test
+        // We save the values for the new note.
         pressBack();
+
+        /*
+            Logic Behaviour Check: Check that create note logic is run properly.
+
+            GOAL: Make sure that whatever note is the last in the list corresponds to all the values
+            we put in this input screen.
+
+         */
+
+        // Get index at the last note in the list
+        int noteIndex = dataManager.getNotes().size() -1;
+
+        // Use index to get the last note
+        NoteInfo note = dataManager.getNotes().get(noteIndex);
+
+        // Assert methods to make sure values in the note correspond to all the values we put in
+        // the screen.
+        assertEquals(course, note.getCourse());
+        assertEquals(noteTitle, note.getTitle());
+        assertEquals(noteText, note.getText());
+
+
     }
 }
