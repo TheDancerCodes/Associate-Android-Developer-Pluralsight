dce6270d27c3cb726ee04bb547c5f0d61658c203
Roger Taracha <rojtaracha@gmail.com>
2019-06-19 08:33:13 +0300

    Implementing Basic Test Interaction: [+] Tetsing the flow of creating a new note.
    [+] Using the 2 steps: Locate view & perform action.
    
    [+] TIP: Make sure everything we need for our test environment is marked public.

--- 2-android-tools-and-testing/NoteKeeper/app/src/androidTest/java/com/thedancercodes/notekeeper/NoteCreationTest.java
+++ 2-android-tools-and-testing/NoteKeeper/app/src/androidTest/java/com/thedancercodes/notekeeper/NoteCreationTest.java
@@ -1,5 +1,6 @@
 package com.thedancercodes.notekeeper;
 
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -7,13 +8,24 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+/* TIP: Add static imports for the Espresso, ViewMatchers & ViewActions class.
+
+   Allow us to use any static methods from these classes as just method names.
+
+   Makes our test code more readable.
+
+ */
 import static org.junit.Assert.*;
+import static android.support.test.espresso.Espresso.*;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 
 @RunWith(AndroidJUnit4.class)
 public class NoteCreationTest {
 
     /**
-     * Get the activity on which to run the test.
+     * Get the activity on which to run the test/launch.
      * <p>
      * Specify the activity where the test starts.
      * <p>
@@ -21,12 +33,28 @@ public class NoteCreationTest {
      * activity instance for us before each test & cleaning it up after each test.
      */
     @Rule
-    ActivityTestRule<NoteListActivity> noteListActivityRule =
+    public ActivityTestRule<NoteListActivity> noteListActivityRule =
             new ActivityTestRule<>(NoteListActivity.class);
 
     @Test
     public void createNewNote() {
 
+        // Local Variable to get a specific View
+        // ViewInteraction fabNewNote = onView(withId(R.id.fab));
+
+        // Perform an action on the View
+        // fabNewNote.perform(click());
+
+        // Simplified Version of the 2 steps above 👆
+        onView(withId(R.id.fab)).perform(click());
+
+        // Get to our note title field of the NoteActivity & input text
+        onView(withId(R.id.text_note_title)).perform(typeText("Test Note Title"));
+
+        // Get to our note text field of the NoteActivity & input text
+        onView(withId(R.id.text_note_text))
+                .perform(typeText("This is the body of our test note"),
+                        closeSoftKeyboard());
     }
 
 }
