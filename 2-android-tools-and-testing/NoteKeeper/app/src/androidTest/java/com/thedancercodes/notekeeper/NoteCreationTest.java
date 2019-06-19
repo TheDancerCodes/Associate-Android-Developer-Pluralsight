b5bd1b5f9872468732f8e2eda34fab299e2a93e0
Roger Taracha <rojtaracha@gmail.com>
2019-06-19 09:59:56 +0300

    Implementing Spinner & Back Button Behaviour: [+] Use onData method to select a course from the spinner.
    [+] TIP: For adapterView derived views, test selection based on target data.
        [=] Use Espresso class onData method: Specify a matcher based on our target data rather than a view.
    
        [=] Use the general purpose hamcrest matchers
    
        [=] Do things tied to data matching and not view matching.

--- 2-android-tools-and-testing/NoteKeeper/app/src/androidTest/java/com/thedancercodes/notekeeper/NoteCreationTest.java
+++ 2-android-tools-and-testing/NoteKeeper/app/src/androidTest/java/com/thedancercodes/notekeeper/NoteCreationTest.java
@@ -4,6 +4,7 @@ import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,9 +21,20 @@ import static android.support.test.espresso.Espresso.*;
 import static android.support.test.espresso.matcher.ViewMatchers.*;
 import static android.support.test.espresso.action.ViewActions.*;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static org.hamcrest.Matchers.*;
+import static android.support.test.espresso.Espresso.pressBack;
 
 @RunWith(AndroidJUnit4.class)
 public class NoteCreationTest {
+
+    // Static member variable that points to the DataManager
+    static DataManager dataManager;
+
+    // Initialize DataManager
+    @BeforeClass
+    public static void classSetUp() {
+        dataManager = DataManager.getInstance();
+    }
 
     /**
      * Get the activity on which to run the test/launch.
@@ -39,22 +51,39 @@ public class NoteCreationTest {
     @Test
     public void createNewNote() {
 
+        // Initialize variable that points to the course we want to use in our test
+        final CourseInfo course = dataManager.getCourse("java_lang");
+
+        // Variable for note title & text
+        final String noteTitle = "Test Note Title";
+        final String noteText = "This is the body of our test note";
+
         // Local Variable to get a specific View
         // ViewInteraction fabNewNote = onView(withId(R.id.fab));
 
         // Perform an action on the View
         // fabNewNote.perform(click());
 
-        // Simplified Version of the 2 steps above 👆
+        // Simplified Version of the 2 steps above 👆: Launches NoteActivity
         onView(withId(R.id.fab)).perform(click());
 
+        // Click on the spinner
+        onView(withId(R.id.spinner_courses)).perform(click());
+
+        // Make a selection from the spinner.
+        // Look through the AdapterViews on this Activity, find the one that holds
+        // CourseInfo instance which is equal to the course variable & select it.
+        onData(allOf(instanceOf(CourseInfo.class), equalTo(course))).perform(click());
+
         // Get to our note title field of the NoteActivity & input text
-        onView(withId(R.id.text_note_title)).perform(typeText("Test Note Title"));
+        onView(withId(R.id.text_note_title)).perform(typeText(noteTitle));
 
         // Get to our note text field of the NoteActivity & input text
         onView(withId(R.id.text_note_text))
-                .perform(typeText("This is the body of our test note"),
+                .perform(typeText(noteText),
                         closeSoftKeyboard());
-    }
 
+        // Leave NoteActivity at the end of the test
+        pressBack();
+    }
 }
