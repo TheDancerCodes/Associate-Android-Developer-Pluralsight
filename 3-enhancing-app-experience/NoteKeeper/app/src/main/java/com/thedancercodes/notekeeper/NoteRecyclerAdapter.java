1d5d7875f44419afbb6a8b03bef7a6dbe3503fbc
Roger Taracha <rojtaracha@gmail.com>
2019-06-21 07:38:11 +0300

    View Management Within a RecyclerView: [+] ViewHolder class that receives a View, get references to the TextViews inside it.
    [+] onCreateViewHolder method inflates our View, passes it into our ViewHolder class & returns it back so it can go into the pool the RecyclerView uses.

--- 3-enhancing-app-experience/NoteKeeper/app/src/main/java/com/thedancercodes/notekeeper/NoteRecyclerAdapter.java
+++ 3-enhancing-app-experience/NoteKeeper/app/src/main/java/com/thedancercodes/notekeeper/NoteRecyclerAdapter.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder> {
 
@@ -23,13 +24,18 @@ public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapte
     // Creates our ViewHolder instances. It also creates the views themselves
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+
+        // Creating the View
+        View itemView = layoutInflater.inflate(R.layout.item_note_list, viewGroup, false);
+
+        // Return a new instance of ViewHolder class
+        return new ViewHolder(itemView);
     }
 
     // Associates data with our views
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
 
     }
 
@@ -39,10 +45,22 @@ public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapte
         return 0;
     }
 
-    // Holds information for individual views
+    // Holds information for individual views.
+    // This ViewHolder is supposed to keep references to any views we are going to set at runtime
+    // for each item.
     public class ViewHolder extends RecyclerView.ViewHolder {
+
+        // We make these fields public so that our outer class (NoteRecyclerAdapter), can
+        // reference these fields directly
+        public final TextView textCourse;
+        public final TextView textTitle;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            // Get references to each of the TextViews within our layout
+            textCourse = itemView.findViewById(R.id.text_course);
+            textTitle = itemView.findViewById(R.id.text_title);
         }
     }
 
