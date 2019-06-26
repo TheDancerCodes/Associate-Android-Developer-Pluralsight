e6603335e67ba447a0de892442ff2687ef10caa3
Roger Taracha <rojtaracha@gmail.com>
2019-06-26 09:07:55 +0300

    Testing Navigation Drawer & RecyclerView: [+] Test that we can successfully select notes from navigation drawer,
    [+] Select the first item from the RecyclerView,
    
    [+] Then verify that the NoteActivity has the correct values for that first note.

--- 3-enhancing-app-experience/NoteKeeper/app/src/androidTest/java/com/thedancercodes/notekeeper/NextThroughNotesTest.java
+++ 3-enhancing-app-experience/NoteKeeper/app/src/androidTest/java/com/thedancercodes/notekeeper/NextThroughNotesTest.java
@@ -4,6 +4,8 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import android.support.test.espresso.contrib.DrawerActions;
+import android.support.test.espresso.contrib.NavigationViewActions;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 
 import static android.support.test.espresso.Espresso.onView;
@@ -29,9 +31,34 @@ public class NextThroughNotesTest {
     @Test
     public void NextThroughNotes() {
 
-        // Get DrawerLayout View & open the navigation drawer
+        // Reference to DrawerLayout View & open the navigation drawer
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
 
+        // Reference to NavigationView under DrawerLayout & select the navigation option for notes
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_notes));
+
+        // Reference to RecyclerView & select first note within it.
+        // Result of this is to open up the NoteActivity.
+        onView(withId(R.id.list_items)).perform(RecyclerViewActions
+                .<NoteRecyclerAdapter.ViewHolder>actionOnItemAtPosition(0, click()));
+
+        /* Verify that we have the values we expect in the NoteActivity */
+
+        // Reference to NotesList from our Manager
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        int index = 0;
+
+        // Get first note from the Note List
+        NoteInfo note = notes.get(index);
+
+        /* Walk through each of the UI fields on the NoteActivity & verify that the Spinner
+           and TextViews have the right text for this note in them. */
+        onView(withId(R.id.spinner_courses)).check(
+                matches(withSpinnerText(note.getCourse().getTitle())));
+
+        onView(withId(R.id.text_note_title)).check(matches(withText(note.getTitle())));
+
+        onView(withId(R.id.text_note_text)).check(matches(withText(note.getText())));
     }
 
 }
