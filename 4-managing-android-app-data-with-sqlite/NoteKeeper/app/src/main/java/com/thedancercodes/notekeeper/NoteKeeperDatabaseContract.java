51c1114272d0b4515e64dc5168a09916afe74fb8
Roger Taracha <rojtaracha@gmail.com>
2019-07-02 21:31:02 +0300

    Implementing the Contract Class.

---
+++ 4-managing-android-app-data-with-sqlite/NoteKeeper/app/src/main/java/com/thedancercodes/notekeeper/NoteKeeperDatabaseContract.java
@@ -0,0 +1,38 @@
+package com.thedancercodes.notekeeper;
+
+public final class NoteKeeperDatabaseContract {
+
+    private NoteKeeperDatabaseContract() {
+    } // Make non-creatable (Private Constructor)
+
+    /*
+      Nested Classes for each of the tables
+     */
+    public static final class CourseInfoEntry {
+        public static final String TABLE_NAME = "course_info";
+        public static final String COLUMN_COURSE_ID = "course_id";
+        public static final String COLUMN_COURSE_TITLE = "course_title";
+
+        // Constant to create the table
+        public static final String SQL_CREATE_TABLE =
+                "CREATE TABLE " + TABLE_NAME + " (" +
+                        COLUMN_COURSE_ID + ", " +
+                        COLUMN_COURSE_TITLE + ")";
+    }
+
+    public static final class NoteInfoEntry {
+
+        public static final String TABLE_NAME = "note_info";
+        public static final String COLUMN_NOTE_TITLE = "note_title";
+        public static final String COLUMN_NOTE_TEXT = "note_text";
+        public static final String COLUMN_COURSE_ID = "course_id";
+
+        // Constant to create the table
+        public static final String SQL_CREATE_TABLE =
+                "CREATE TABLE " + TABLE_NAME + " (" +
+                        COLUMN_NOTE_TITLE + ", " +
+                        COLUMN_NOTE_TEXT + ", " +
+                        COLUMN_COURSE_ID + ")";
+
+    }
+}
