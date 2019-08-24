1fa576b84a6542bfefafaa46eceb3c77de164079
Roger Taracha <rojtaracha@gmail.com>
2019-08-24 19:13:36 +0300

    Creating a Job Implementation Class: [+] Add a feature to the app that allows users to perform an upload of their notes data.
    [+] NoteUploader class - Simulates the work of uploading data from our content provider’s notes table
    
    [+] Create the JobService & override the required methods. (NoteUploaderJobService)
    
    [+] Provide require permission in the manifest file.

---
+++ 7-leveraging-the-power-of-the-android-platform/NoteKeeper/app/src/main/java/com/thedancercodes/notekeeper/NoteUploaderJobService.java
@@ -0,0 +1,32 @@
+package com.thedancercodes.notekeeper;
+
+import android.app.Service;
+import android.app.job.JobInfo;
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.PersistableBundle;
+
+public class NoteUploaderJobService extends JobService {
+    public NoteUploaderJobService() {
+    }
+
+    /**
+     * Called to indicate that the job has begun executing.
+     */
+    @Override
+    public boolean onStartJob(JobParameters params) {
+        return false;
+    }
+
+    /**
+     * This method is called if the system has determined that you must stop execution of your job
+     * even before you've had a chance to call {@link #jobFinished(JobParameters, boolean)}.
+     */
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        return false;
+    }
+
+}
