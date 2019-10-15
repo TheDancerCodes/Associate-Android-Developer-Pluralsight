e468370d1d955d4c9ef7240ffb674a34d7c2798f
Roger Taracha <rojtaracha@gmail.com>
2019-10-15 19:07:53 +0300

    Animating Vector Drawables: {+} Check-To-Close Animation.

--- Kotlin/AnimationsDemo/app/src/main/java/com/thedancercodes/animationsdemo/MainActivity.kt
+++ Kotlin/AnimationsDemo/app/src/main/java/com/thedancercodes/animationsdemo/MainActivity.kt
@@ -1,13 +1,36 @@
 package com.thedancercodes.animationsdemo
 
+import android.graphics.drawable.AnimatedVectorDrawable
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /* Code to implement animation */
+
+        // setOnClickListener on ImageView
+        avdImage.setOnClickListener {
+            checkToClose()
+        }
+
+    }
+
+    private fun checkToClose() {
+
+        // Set animated vector as image resource
+        avdImage.setImageResource(R.drawable.avd_check_to_close)
+
+        // Get drawable from ImageView, cast it to AnimatedVectorDrawable & assign it to
+        // variable avdCheckToClose
+        val avdCheckToClose = avdImage.drawable as AnimatedVectorDrawable
+
+        // Start Animation
+        avdCheckToClose.start()
 
     }
 }
