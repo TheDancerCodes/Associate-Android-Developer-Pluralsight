dccd3ebf0d294ba709b0525eb239a0c5458a6493
Roger Taracha <rojtaracha@gmail.com>
2019-10-15 13:22:00 +0300

    Listening to Animation Events.

--- Kotlin/AnimationsDemo/app/src/main/java/com/thedancercodes/animationsdemo/MainActivity.kt
+++ Kotlin/AnimationsDemo/app/src/main/java/com/thedancercodes/animationsdemo/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.thedancercodes.animationsdemo
 
+import android.animation.Animator
 import android.animation.AnimatorInflater
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Animator.AnimatorListener {
+
+    // Variables as Class Properties
+    private var rotateAnimator: Animator? = null
+    private var translateAnimator: Animator? = null
+    private var scaleAnimator: Animator? = null
+    private var fadeAnimator: Animator? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,9 +24,10 @@ class MainActivity : AppCompatActivity() {
 
     fun rotateAnimation(view: View) {
 
-        val rotateAnimator = AnimatorInflater.loadAnimator(this, R.animator.rotate)
+        rotateAnimator = AnimatorInflater.loadAnimator(this, R.animator.rotate)
         rotateAnimator?.apply {
             setTarget(targetImage)
+            addListener(this@MainActivity)
             start()
         }
 
@@ -25,28 +35,66 @@ class MainActivity : AppCompatActivity() {
 
     fun scaleAnimation(view: View) {
 
-        val scaleAnimator = AnimatorInflater.loadAnimator(this, R.animator.scale)
+        scaleAnimator = AnimatorInflater.loadAnimator(this, R.animator.scale)
         scaleAnimator?.apply {
             setTarget(targetImage)
+            addListener(this@MainActivity)
             start()
         }
     }
 
     fun translateAnimation(view: View) {
 
-        val translateAnimator = AnimatorInflater.loadAnimator(this, R.animator.translate)
+        translateAnimator = AnimatorInflater.loadAnimator(this, R.animator.translate)
         translateAnimator?.apply {
             setTarget(targetImage)
+            addListener(this@MainActivity)
             start()
         }
     }
 
     fun fadeAnimation(view: View) {
 
-        val fadeAnimator = AnimatorInflater.loadAnimator(this, R.animator.alpha)
+        fadeAnimator = AnimatorInflater.loadAnimator(this, R.animator.alpha)
         fadeAnimator?.apply {
             setTarget(targetImage)
+            addListener(this@MainActivity)
             start()
         }
+    }
+
+    // Implementation of AnimatorListener interface
+    override fun onAnimationStart(animation: Animator?) {
+
+        if (animation == rotateAnimator)
+            Toast.makeText(this, "Rotate Animation Started", Toast.LENGTH_SHORT).show()
+
+        if (animation == scaleAnimator)
+            Toast.makeText(this, "Scale Animation Started", Toast.LENGTH_SHORT).show()
+
+        if (animation == translateAnimator)
+            Toast.makeText(this, "Translate Animation Started", Toast.LENGTH_SHORT).show()
+
+        if (animation == fadeAnimator)
+            Toast.makeText(this, "Fade Animation Started", Toast.LENGTH_SHORT).show()
+
+    }
+
+    override fun onAnimationRepeat(animation: Animator?) {
+
+        Toast.makeText(this, "Animation Repeated", Toast.LENGTH_SHORT).show()
+
+    }
+
+    override fun onAnimationEnd(animation: Animator?) {
+
+        Toast.makeText(this, "Animation Ended", Toast.LENGTH_SHORT).show()
+
+    }
+
+    override fun onAnimationCancel(animation: Animator?) {
+
+        Toast.makeText(this, "Animation Cancelled", Toast.LENGTH_SHORT).show()
+
     }
 }
