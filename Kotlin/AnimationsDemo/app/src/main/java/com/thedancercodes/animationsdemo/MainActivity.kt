a979441a6eb0cbeffe149c6c922258b9ac2a4722
Roger Taracha <rojtaracha@gmail.com>
2019-10-16 11:03:11 +0300

    Rectify crash for AnimatedVectorDrawableCompat.

--- Kotlin/AnimationsDemo/app/src/main/java/com/thedancercodes/animationsdemo/MainActivity.kt
+++ Kotlin/AnimationsDemo/app/src/main/java/com/thedancercodes/animationsdemo/MainActivity.kt
@@ -35,7 +35,7 @@ class MainActivity : AppCompatActivity() {
 
         // Get drawable from ImageView, cast it to AnimatedVectorDrawable & assign it to
         // variable avdCheckToClose
-        val avdCheckToClose = avdImage.drawable as AnimatedVectorDrawableCompat
+        val avdCheckToClose = avdImage.drawable as AnimatedVectorDrawable
 
         // Start Animation
         avdCheckToClose.start()
@@ -48,7 +48,7 @@ class MainActivity : AppCompatActivity() {
 
         // Get drawable from ImageView, cast it to AnimatedVectorDrawable & assign it to
         // variable avdCheckToClose
-        val avdCheckToClose = avdImage.drawable as AnimatedVectorDrawableCompat
+        val avdCheckToClose = avdImage.drawable as AnimatedVectorDrawable
 
         // Start Animation
         avdCheckToClose.start()
