30b61d1be1e9c642db9d1a40517d9150cb39cc77
Roger Taracha <rojtaracha@gmail.com>
2019-10-16 15:38:34 +0300

    Apply Transitions from Code

--- Kotlin/AnimationsDemo/app/src/main/java/com/thedancercodes/animationsdemo/MainActivity.kt
+++ Kotlin/AnimationsDemo/app/src/main/java/com/thedancercodes/animationsdemo/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var scene2: Scene
     private lateinit var currentScene: Scene
     private lateinit var transition: Transition
+    private lateinit var transitionSet: TransitionSet
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +25,27 @@ class MainActivity : AppCompatActivity() {
 
 
         // Step 2: Create a Transition object to define what type of animation you want
-        transition = TransitionInflater.from(this).inflateTransition(R.transition.example_2)
+        // transition = TransitionInflater.from(this).inflateTransition(R.transition.example_2)
+
+        val cbTransition = ChangeBounds()
+        cbTransition.duration = 500
+        cbTransition.interpolator = LinearInterpolator()
+
+        val fadeInTransition = Fade(Fade.IN)
+        fadeInTransition.duration = 250
+        fadeInTransition.startDelay = 400
+        fadeInTransition.addTarget(R.id.txvTitle)
+
+        val fadeOutTransition = Fade(Fade.OUT)
+        fadeOutTransition.duration = 50
+        fadeOutTransition.addTarget(R.id.txvTitle)
+
+        transitionSet = TransitionSet()
+        transitionSet.ordering = TransitionSet.ORDERING_TOGETHER
+
+        transitionSet.addTransition(cbTransition)
+        transitionSet.addTransition(fadeInTransition)
+        transitionSet.addTransition(fadeOutTransition)
 
         scene1.enter()
         currentScene = scene1
@@ -34,10 +55,10 @@ class MainActivity : AppCompatActivity() {
 
         // Step 3: Call TransitionManager.go() to run animation
         currentScene = if (currentScene === scene1) {
-            TransitionManager.go(scene2, transition)
+            TransitionManager.go(scene2, transitionSet)
             scene2
         } else {
-            TransitionManager.go(scene1, transition)
+            TransitionManager.go(scene1, transitionSet)
             scene1
         }
 
