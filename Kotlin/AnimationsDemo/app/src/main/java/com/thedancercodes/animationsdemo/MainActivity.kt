d3255035523ea3f8f92a95890340b3ba2e9b9141
Roger Taracha <rojtaracha@gmail.com>
2020-01-15 17:52:35 +0300

    Using Open Source Library: RecyclerView Animations PT1 {+} Project Steup.

--- Kotlin/AnimationsDemo/app/src/main/java/com/thedancercodes/animationsdemo/MainActivity.kt
+++ Kotlin/AnimationsDemo/app/src/main/java/com/thedancercodes/animationsdemo/MainActivity.kt
@@ -3,6 +3,8 @@ package com.thedancercodes.animationsdemo
 import android.os.Bundle
 import android.support.constraint.ConstraintSet
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutManager
 import android.transition.ChangeBounds
 import android.transition.TransitionManager
 import android.view.animation.AnticipateInterpolator
@@ -12,47 +14,24 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var isDetailLayout = false
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /**
-         * NOTE: constraintLayout is the id of the ConstrainTLayout present in both
-         * activity_main and activity_main_detail.
-          */
-
-        constraintLayout.setOnClickListener {
-            if (isDetailLayout)
-            swapFrames(R.layout.activity_main) // switch to default layout
-            else
-            swapFrames(R.layout.activity_main_detail) // switch to detail layout
-        }
+        setUpRecyclerView()
     }
 
-    // Function to swap layouts or transition between layouts.
-    private fun swapFrames(layoutId: Int){
+    private fun setUpRecyclerView() {
 
-        // Instantiate ConstraintSet object
-        val constraintSet = ConstraintSet()
+        val adapter = RecyclerAdapter(this, Landscape.data)
+        recyclerView.adapter = adapter
 
-        // Get all the constraints of the child views of the new layout; which will replace 
-        // the old one.
-        constraintSet.clone(this, layoutId)
+        val layoutManager = LinearLayoutManager(this)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        recyclerView.layoutManager = layoutManager
 
-        // Adding effect to transition
-        val transition = ChangeBounds()
-        transition.interpolator = AnticipateOvershootInterpolator(1.0f)
-        transition.duration = 1200
-
-        // Animate
-        TransitionManager.beginDelayedTransition(constraintLayout, transition)
-
-        // Apply new constraints to our root layout
-        constraintSet.applyTo(constraintLayout)
-
-        // Toggle the Boolean value
-        isDetailLayout = !isDetailLayout
+        recyclerView.itemAnimator = DefaultItemAnimator()
     }
 }
+
+
