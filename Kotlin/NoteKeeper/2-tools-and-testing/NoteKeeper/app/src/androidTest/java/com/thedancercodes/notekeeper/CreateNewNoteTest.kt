9d3e0626d23e408877462250bd78c3a848a23e7d
Roger Taracha <rojtaracha@gmail.com>
2020-05-18 12:29:43 +0300

    Implementing the Automated UI Test: {+} Leverage ActivityTestRule. {+} Add missing dependency.

--- Kotlin/NoteKeeper/2-tools-and-testing/NoteKeeper/app/src/androidTest/java/com/thedancercodes/notekeeper/CreateNewNoteTest.kt
+++ Kotlin/NoteKeeper/2-tools-and-testing/NoteKeeper/app/src/androidTest/java/com/thedancercodes/notekeeper/CreateNewNoteTest.kt
@@ -4,9 +4,20 @@ import android.support.test.runner.AndroidJUnit4
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
+import android.support.test.espresso.Espresso.*
+import android.support.test.espresso.matcher.ViewMatchers.*
+import org.hamcrest.Matchers.*
+import android.support.test.espresso.action.ViewActions.*
+import android.support.test.rule.ActivityTestRule
+import org.junit.Rule
+
 
 @RunWith(AndroidJUnit4::class)
 class CreateNewNoteTest {
+
+    // ActivityTestRule ensures that the NoteListActivity is up & running.
+    @Rule @JvmField
+    val noteListActivity = ActivityTestRule(NoteListActivity::class.java)
 
     @Test
     fun createNewNote() {
