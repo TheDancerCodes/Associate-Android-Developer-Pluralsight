c9c138dd94e632ced1fe2cdda65c9c9b7b570c86
Roger Taracha <rojtaracha@gmail.com>
2020-05-18 14:03:49 +0300

    Implementing Spinner and Back Button Interactions

--- Kotlin/NoteKeeper/2-tools-and-testing/NoteKeeper/app/src/androidTest/java/com/thedancercodes/notekeeper/CreateNewNoteTest.kt
+++ Kotlin/NoteKeeper/2-tools-and-testing/NoteKeeper/app/src/androidTest/java/com/thedancercodes/notekeeper/CreateNewNoteTest.kt
@@ -10,6 +10,7 @@ import org.hamcrest.Matchers.*
 import android.support.test.espresso.action.ViewActions.*
 import android.support.test.rule.ActivityTestRule
 import org.junit.Rule
+import android.support.test.espresso.Espresso.pressBack
 
 
 @RunWith(AndroidJUnit4::class)
@@ -22,15 +23,24 @@ class CreateNewNoteTest {
     @Test
     fun createNewNote() {
 
+        val course = DataManager.courses["android_async"]
         val noteTitle = "Test note title"
         val noteText = "This is the body of our test note"
 
         // Identify the FAB & click on it
         onView(withId(R.id.fab)).perform(click())
 
+        // Tap the spinner to present the selections
+        onView(withId(R.id.fab)).perform(click())
+
+        // Make a selection inside the course spinner
+        onData(allOf(instanceOf(CourseInfo::class.java), equalTo(course))).perform(click())
+
         // Add note title & text to MainActivity
         onView(withId(R.id.textNoteTitle)).perform(typeText(noteTitle))
         onView(withId(R.id.textNoteText)).perform(typeText(noteText))
 
+        // Press the back button
+        pressBack()
     }
 }
