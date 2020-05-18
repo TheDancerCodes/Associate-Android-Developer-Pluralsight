7790cb15573a3c596d9bc464cb1d9412e139809f
Roger Taracha <rojtaracha@gmail.com>
2020-05-18 12:01:32 +0300

    Setting up the Test: {+} Create the test class & test function.

---
+++ Kotlin/NoteKeeper/2-tools-and-testing/NoteKeeper/app/src/androidTest/java/com/thedancercodes/notekeeper/CreateNewNoteTest.kt
@@ -0,0 +1,15 @@
+package com.thedancercodes.notekeeper
+
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class CreateNewNoteTest {
+
+    @Test
+    fun createNewNote() {
+
+    }
+}
